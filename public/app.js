fee2937a46a49df7defda1149c9391ad2caf5a9a
Teemu Sarapisto <tsarpf@gmail.com>
2016-09-25 23:44:25 +0300

    Further front-end dev

--- public/app.js
+++ public/app.js
@@ -1,12 +1,37 @@
-console.log('hello world')
+window.onload = () => {
+  console.log('hello world')
 
-Rx.DOM.post('/poem', { text: 'sometext' })
-  .subscribe(
-    function (data) {
-      console.log(data.response)
+  const input = document.querySelector('div.body-container textarea[name=poem]')
+  const submit = document.querySelector('button[name=submit]')
+  const charLeft = document.querySelector('#char-left')
+
+  const inputChange = Rx.DOM.input(input)
+  const submitClick = Rx.Observable.fromEvent(submit, 'click')
+
+  const changeSub = inputChange.subscribe(
+    () => {
+      charLeft.innerHTML = 140 - input.value.length
     },
-    function (err) {
-      console.log(err)
-      // Log the error
-    }
+    (error) => console.log(error),
+    () => console.log('input changes finished')
   )
+
+  const clickSub = submitClick.subscribe(
+    () => {
+      Rx.DOM.post('/poem', {poem: input.value})
+        .subscribe(
+          function (data) {
+            console.log('got data')
+            console.log(data.response)
+          },
+          function (err) {
+            console.log(err)
+            // Log the error
+          }
+        )
+    },
+    (error) => console.log(error),
+    () => console.log('clicking finished')
+  )
+
+}
